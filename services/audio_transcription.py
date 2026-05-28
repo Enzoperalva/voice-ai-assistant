e1c7d779e2a9d92ec72993058eb65fe31bb6c9df
Enzo Pinheiro Peralva <pessoalenzopinheiro@gmail.com>
2026-05-28 09:48:19 -0300

    remove with open file and added in main.py

--- services/audio_transcription.py
+++ services/audio_transcription.py
@@ -9,7 +9,7 @@ client = genai.Client(
     api_key=TOKEN
 )
 
-def transcibe_with_gemini(audio_path: str, model: str = "gemini-3.5-flash", prompt: str | None = None) -> str:
+def transcribe_with_gemini(audio_path: str, model: str = "gemini-3.5-flash", prompt: str | None = None) -> str:
     audio_file = client.files.upload(file=audio_path)
     
     response = client.models.generate_content(
@@ -21,12 +21,3 @@ def transcibe_with_gemini(audio_path: str, model: str = "gemini-3.5-flash", prom
     )
 
     return response.text
-
-
-with open("data/prompt.txt", "r", encoding='utf-8') as arq:
-    prompt = arq.read()
-
-prompt_user = transcibe_with_gemini(
-    audio_path="data/audio_for_ai.wav",
-    prompt=prompt
-)
